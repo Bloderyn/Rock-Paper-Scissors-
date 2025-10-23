d3966b1d7b2e0d842d141dffd815bed74f869b45
Bloderyn <Mtieres@gmail.com>
2025-10-23 15:35:41 +0200

    Ended the exercice without the loop knowledge that'll come after

--- index.js
+++ index.js
@@ -14,3 +14,47 @@ function getHumanChoice() {
    const choice = prompt("Rock, Paper, or Scissors?");
         return choice;
 }
+
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+    const humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!");
+    } 
+    else if (humanChoice === "rock" && computerChoice === "scissors") ||
+            (humanChoice === "paper" && computerChoice === "rock") ||
+            (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log (`${humanChoice} beats ${computerChoice}. You win this round!`)
+        humanScore++;
+    } 
+    else {
+        console.log (`${computerChoice} beats ${humanChoice}. You lose this round!`)
+        computerScore++;
+        }
+    }
+
+    // Simulate 5 rounds of the game - no loop learnt yet
+    playRound ("rock", "scissors");
+    playRound ("rock", "paper");
+    playRound ("paper", "scissors");
+    playRound ("scissors", "paper");
+    playRound ("paper", "rock");
+
+    console.log(`Final Scores - Human: ${humanScore}, Computer: ${computerScore}`);
+
+    if (humanScore > computerScore) {
+        console.log("You win the game!");
+    } else if (computerScore > humanScore) {
+        console.log("Computer wins the game!");
+    } else (humanScore === computerScore) {
+        console.log("The game is a tie!");
+    }
+
+}
+
+playGame();
